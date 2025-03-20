582428c5ef64993955eec77e378d12ae8327b265
Kevin Otieno <otienokevin1000090@gmail.com>
2025-03-20 21:31:00 +0300

    commit logins.h

--- .login_credentials/loginsq.h
+++ .login_credentials/loginsq.h
@@ -1,1 +1,15 @@
 
+// loginsq.h
+
+#ifndef LOGIN_SQ_H
+#define LOGIN_SQ_H
+
+// Maximum length for username and password
+#define MAX_USERNAME_LENGTH 256
+#define MAX_PASSWORD_LENGTH 256
+
+// Function declarations
+void store_credentials(const char *username, const char *password);
+int verify_credentials(const char *username, const char *password);
+
+#endif // LOGIN_SQ_H
